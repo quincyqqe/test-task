0c69bb1c9d0227efbbe5cb4dade8ad389d167a0c
Maksym Kryvenko <111203167+quincyqqe@users.noreply.github.com>
2024-04-23 14:24:01 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,11 +5,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(cors()) // Позволяет CORS запросы от любого источника
-
+app.use(cors())
 app.use(express.json())
 
-// Конфигурация CORS заголовков
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
