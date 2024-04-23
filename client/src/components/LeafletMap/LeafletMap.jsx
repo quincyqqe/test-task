48e0b89affca3bb661fb72e3fa75af0da3b2da45
sxcred <111203167+quincyqqe@users.noreply.github.com>
2024-04-23 12:21:34 +0200

    vercel

--- client/src/components/LeafletMap/LeafletMap.jsx
+++ client/src/components/LeafletMap/LeafletMap.jsx
@@ -23,7 +23,7 @@ const LeafletMap = () => {
 	}, [])
 
 	const loadMarkersFromServer = () => {
-		fetch('https://test-task-server-six.vercel.app/')
+		fetch('http://localhost:3000/')
 			.then(response => response.json())
 			.then(data => {
 				setMarkers(data)
